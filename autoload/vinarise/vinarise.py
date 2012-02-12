6e929b5afbdd6aac87ad19a6bcc2bf1e138ca05e
Shougo Matsushita <Shougo.Matsu@gmail.com>
2012-02-12 20:53:52 +0900

    - Fixed python script.

--- autoload/vinarise/vinarise.py
+++ autoload/vinarise/vinarise.py
@@ -9,12 +9,12 @@ class VinariseBuffer:
         self.file = open(path, 'rb')
         mmap_max = 0
 
-        if is_windows:
+        if int(is_windows):
             self.mmap = mmap.mmap(self.file.fileno(), mmap_max,
                     None, mmap.ACCESS_COPY, 0)
         else:
             self.mmap = mmap.mmap(self.file.fileno(), mmap_max,
-                    mmap.MAP_PRIVATE, mmap.PROT_READ | mmap.PROT_WRITE, mmap.ACCESS_COPY, 0)
+                    mmap.ACCESS_COPY, 0)
     def close(self):
         self.file.close()
         self.mmap.close()
